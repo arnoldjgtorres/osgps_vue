d26386a7565bf5f2241de6fa1e44b72156c5c703
27017232 <arnoldjgtorres@gmail.com>
2020-02-11 02:36:14 -0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,71 @@
+<template>
+  <div id="app">
+    <!--
+    <AddTodo v-on:add-todo="addTodo" />
+    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
+    {{ todos }}
+    -->
+    <h2>Vue / Go Project</h2>
+    <button v-on:click="accessAPI">Retrieve Data</button>
+    <Display v-bind:content="content" />
+  </div>
+</template>
+
+<script>
+import Display from "./components/Display";
+import axios from "axios";
+//axios.defaults.headers.common["Access-Control-Allow-Origin"] = "*";
+export default {
+  name: "app",
+  components: {
+    //Todos,
+    //AddTodo,
+    Display
+  },
+  data() {
+    return {
+      todos: [],
+      content: null
+    };
+  },
+  methods: {
+    /*
+    deleteTodo(id) {
+      axios
+        .delete(`http://jsonplaceholder.typicode.com/todos/${id}`)
+        .then(res => (this.todos = this.todos.filter(todo => todo.id !== id)))
+        .catch(err => console.log(err));
+    },
+    addTodo(newTodo) {
+      const { title, completed } = newTodo;
+      axios
+        .post("http://jsonplaceholder.typicode.com/todos", {
+          title,
+          completed
+        })
+        .then(res => console.log(res.data))
+        .catch(err => console.log(err));
+    },*/
+    accessAPI() {
+      axios
+        //.get("http://jsonplaceholder.typicode.com/todos?_limit=5")
+        .get("https://osgps-server.herokuapp.com/")
+        //.then(res => (this.todos = res.data))
+        .then(res => {
+          this.content = res.data;
+          console.log(res);
+        })
+        .catch(err => console.log(err));
+    }
+  }
+};
+</script>
+
+<style>
+h2 {
+  text-align: center;
+}
+button {
+  margin-top: 2%;
+}
+</style>
---
+++ src/components/Display.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <button v-on:click="clearContent">Clear Data</button>
+    <pre>{{ content | pretty }}</pre>
+  </div>
+</template>
+
+<script>
+import TodoItem from "./TodoItem";
+
+export default {
+  name: "Display",
+  components: {},
+  props: {
+    content: String
+  },
+  methods: {
+    clearContent() {
+      this.content = null;
+    }
+  }
+};
+</script>
+
+<style scoped>
+* {
+  margin-top: 2%;
+  text-align: left;
+}
+</style>
